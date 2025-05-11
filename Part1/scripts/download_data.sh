a49d014ef3fb8cf1b1d77a5f157081673a4505be
Salvatore Gilles Cassarà <gilles2004.sc@gmail.com>
2025-05-11 11:50:59 +0200

    Fixed dataset

--- Part1/scripts/download_data.sh
+++ Part1/scripts/download_data.sh
@@ -5,23 +5,23 @@ mkdir -p Data
 # Array of dataset URLs
 urls=(
     # Diagonal matrices - GPU
-    "https://sparse.tamu.edu/Bodendiek/CurlCurl_4"
+    "https://suitesparse-collection-website.herokuapp.com/MM/Bodendiek/CurlCurl_4.tar.gz"
     "https://suitesparse-collection-website.herokuapp.com/MM/Schenk_AFE/af_shell8.tar.gz"
     # Diagonal matrices - CPU
-    "https://sparse.tamu.edu/Andrews/Andrews"
-    "https://sparse.tamu.edu/Bai/af23560"
+    "https://suitesparse-collection-website.herokuapp.com/MM/Andrews/Andrews.tar.gz"
+    "https://suitesparse-collection-website.herokuapp.com/MM/Bai/af23560.tar.gz"
 
     # Arrowhead matrices - GPU
     "https://suitesparse-collection-website.herokuapp.com/MM/Rajat/rajat31.tar.gz"
     # Arrowhead matrices - CPU
-    "https://sparse.tamu.edu/TKK/cyl6"
+    "https://suitesparse-collection-website.herokuapp.com/MM/TKK/cyl6.tar.gz"
 
     # Unstructured matrices - GPU
     "https://suitesparse-collection-website.herokuapp.com/MM/MAWI/mawi_201512012345.tar.gz"
     "https://suitesparse-collection-website.herokuapp.com/MM/Belcastro/human_gene2.tar.gz"
     # Unstructured matrices - CPU
-    "https://sparse.tamu.edu/AMD/G2_circuit"
-    "https://sparse.tamu.edu/HB/cegb2919"
+    "https://suitesparse-collection-website.herokuapp.com/MM/AMD/G2_circuit.tar.gz"
+    "https://suitesparse-collection-website.herokuapp.com/MM/HB/cegb2919.tar.gz"
 )
 
 # Make sure Data directory exists
